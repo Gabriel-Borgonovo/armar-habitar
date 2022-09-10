25e2a0fcdbf7d16caf2c240be816e7056b06a3b2
Gabriel Borgonovo <gabrielalejandroborgonovo@gmail.com>
2022-09-09 21:11:13 -0300

    Le di funcionamiento al slider con Js

--- js/main.js
+++ js/main.js
@@ -7,3 +7,47 @@ const nav = document.querySelector(`.header_nav`);
 button.addEventListener(`click`, () => {
     nav.classList.toggle(`activado`);
 });
+
+
+//-----CONSTRUCCIÓN DE SLIDER-----------
+
+const slider = document.querySelector(`#carrusel`);
+let sliderSection = document.querySelectorAll(`.carrusel_section`);
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector(`#btn_left`);
+const btnRight = document.querySelector(`#btn_right`);
+
+slider.insertAdjacentElement(`afterbegin`, sliderSectionLast);
+
+function moverDerecha() {
+    let sliderSectionFirst = document.querySelectorAll(`.carrusel_section`)[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all .5s ease";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement(`beforeend`, sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+function moverIzquierda() {
+    let sliderSection = document.querySelectorAll(`.carrusel_section`);
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all .5s ease";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement(`afterbegin`, sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener(`click`, function(){
+    moverDerecha();
+});
+
+btnLeft.addEventListener(`click`, function(){
+    moverIzquierda();
+});
